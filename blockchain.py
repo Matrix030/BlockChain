d3956232a3e298bd4beeb1e47de335fab5606c5a
Rishikesh Gharat <rishigharat@gmail.com>
2021-11-25 21:18:15 +0530

    made some changes

--- blockchain.py
+++ blockchain.py
@@ -47,4 +47,10 @@ class Blockchain:
             block = chain[block_index]
             if block['previous_hash'] != self.hash(previous_block):
                 return False
-            
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+          
+            previous_block = 
